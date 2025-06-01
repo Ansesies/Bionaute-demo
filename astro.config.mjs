84b33fd18d9f2adc509dfae542502a65c0bd71b4
Ansesies <silvaescobarandres@gmail.com>
2025-06-01 17:01:15 -0400

    se agregan configuracion para github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Ansesies.github.io',
+  base: '/Bionaute-demo',
   vite: {
     plugins: [tailwindcss()]
   }
